34e8bf7d4b21b912b71d69925ec751bcc5127f40
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-03-23 15:27:48 +0100

    layout-header

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
-<script></script>
-<template></template>
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+    components: {
+        AppHeader,
+        AppMain,
+        AppFooter
+    }
+}
+</script>
+
+<template>
+    <AppHeader></AppHeader>
+    <AppMain></AppMain>
+    <AppFooter></AppFooter>
+</template>
+
 <style lang="scss" scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,46 @@
+<script>
+
+</script>
+
+<template>
+    <div class="container-header">
+        <div class="logo-header">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <div class="nav-header">
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+                <li>7</li>
+                <li>8</li>
+                <li>9</li>
+                <li>10</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: white;
+    height: 7.1875rem;
+    padding: 0 20.625rem 0 20rem;
+}
+
+ul {
+    display: flex;
+    list-style: none;
+}
+
+ul>li {
+    display: inline-block;
+    margin-right: 1.875rem;
+}
+</style>
